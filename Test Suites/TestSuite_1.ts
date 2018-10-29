5c8432d33fbd4d158ce525a3e0208fe78e9127a5
isuru <isuru@isuru-PC>
2018-10-29 16:00:34 +0530

    Initial commit

---
+++ Test Suites/TestSuite_1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fd8f0d0f-626e-4b28-b130-68d54a74c9b6</testSuiteGuid>
+   <testCaseLink>
+      <guid>d23ef67d-97b6-4876-8e22-985ed0407fed</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
